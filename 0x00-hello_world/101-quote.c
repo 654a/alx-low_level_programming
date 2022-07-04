c931258758927a895e41c29988a0816e5921384f
654a <yonasdesalegn011@gmail.com>
2022-07-04 02:17:20 -0700

    commit 7 for task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,17 +5,14 @@
 
 /**
  *  * main - Entry point
- *    * Return: Always 1 (Success)
+ *   * Return: Always 1 (Success)
  */
 
 int main(void)
 
 {
-char stri[] = "and that piece of art is useful\" - \
 
-	       Dora Korpar, 2015-10-19\n";
-
-	write(2, stri, 59);
-
-		return (1);
+		write(STDOUT_FILENO, "and that piece of art is useful
+				\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
