281013e4fbe3a94abf7e7e7324f58054b72792ef
654a <yonasdesalegn011@gmail.com>
2022-08-01 09:37:27 -0700

    0x0E-structures_typedef task 4 commit 1

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,55 +1,45 @@
-#include "dog.h"
 #include <stdlib.h>
-
+#include "dog.h"
 /**
- *  * new_dog - new element of type struct dog
- *    * @name: name of new dog
- *     * @age: age of new dog
- *      * @owner: owner of new dog
- *       * Return: new struct dog
+ * * new_dog - makes a new struct dog
+ * * @name: his/her name
+ * * @age: how old is he
+ * * @owner: who are you
+ * * Return: new dog
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog;
-char *dog_name, *dog_owner;
-new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
-return (new_dog);
-dog_name = malloc(sizeof(name));
-if (dog_name == NULL)
+unsigned int i, namel, ownerl;
+dog_t *dog;
+if (name == NULL || owner == NULL)
+return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+for (namel = 0; name[namel]; namel++)
+;
+namel++;
+dog->name = malloc(namel * (sizeof(char)));
+if (dog->name == NULL)
 {
-free(new_dog);
+free(dog);
 return (NULL);
 }
-dog_owner = malloc(sizeof(owner));
-if (dog_owner == NULL)
+for (i = 0; i < namel; i++)
+dog->name[i] = name[i];
+dog->age = age;
+for (ownerl = 0; owner[ownerl]; ownerl++)
+;
+ownerl++;
+dog->owner = malloc(ownerl * (sizeof(char)));
+if (dog->owner == NULL)
 {
-free(dog_name);
-free(new_dog);
+free(dog->name);
+free(dog);
 return (NULL);
 }
-_strcpy(dog_name, name);
-_strcpy(dog_owner, owner);
-new_dog->name = dog_name;
-new_dog->owner = dog_owner;
-new_dog->age = age;
-return (new_dog);
-}
-/**
- *  * *_strcpy - copies string to given memory location
- *   * @dest: where the string needs to be copied
- *    * @src: where the string is
- *      * Return: char
- */
-char *_strcpy(char *dest, char *src)
-{
-int len = 0;
-
-while (*(src + len) != '\0')
-{
-*(dest + len) = *(src + len);
-len++;
-}
-*(dest + len) = *(src + len);
-return (dest);
+for (i = 0; i < ownerl; i++)
+dog->owner[i] = owner[i];
+return (dog);
 }
